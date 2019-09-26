f7afbd6eb14af7207362937ee19ef3babd554dcf
Jason Wu <zwu00@stuy.edu>
2019-09-26 10:45:15 -0400

    added makefile

--- main.c
+++ main.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "strings.c"
+#include "strings.h"
 
 int main() {
 	//for testing the custom made string methods
+	printf("DEBUG\n");
+	char first[6] = "Hello";
 }

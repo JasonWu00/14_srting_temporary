ac29aaa5ad6b11c49d52822c86fecffb5f1f60e5
Jason Wu <zwu00@stuy.edu>
2019-09-26 10:55:06 -0400

    test for custom strlen added

--- main.c
+++ main.c
@@ -2,9 +2,13 @@
 #include <stdlib.h>
 //#include "strings.c"
 #include "strings.h"
+#include <string.h>
 
 int main() {
 	//for testing the custom made string methods
 	printf("DEBUG\n");
 	char first[6] = "Hello";
+	int systemOutput = strlen(first); //expecting 5
+	int customOutput = customStrlen(first); //also 5
+	printf("TEST: System strlen returns %i, custom strlen returns %i\n", systemOutput, customOutput);
 }
